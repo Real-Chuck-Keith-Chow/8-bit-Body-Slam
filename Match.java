7f64eaf49165811b63c532fa6ae502efe3f5c33e
Cheuk (Chuck) Chow <rosarollins069@gmail.com>
2025-05-05 21:39:51 -0400

    Add files via upload

---
+++ Match.java
@@ -0,0 +1,10 @@
+package wrestling.simulator;
+
+public class Match {
+	    public static void startMatch(Wrestler player, Wrestler opponent) {
+	        // Turn-based combat logic here
+	        // Use your ASCII ring art
+	    }
+	}
+
+
