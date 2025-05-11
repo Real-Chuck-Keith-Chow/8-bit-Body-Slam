f9bf79267a0e76444653e5cd357b038f6d6d5ea2
Cheuk (Chuck) Chow <rosarollins069@gmail.com>
2025-05-11 16:01:04 -0400

    Update Contract.java

--- Contract.java
+++ Contract.java
@@ -1,8 +1,22 @@
 package wrestling.simulator;
 
-public class Contract {
-	private Promotion promotion;
+
+
+import java.io.Serializable;
+
+class Contract implements Serializable {
+    private Promotion promotion;
     private int payPerShow;
     private int durationWeeks;
-
+    
+    public Contract(Promotion promotion, int payPerShow, int durationWeeks) {
+        this.promotion = promotion;
+        this.payPerShow = payPerShow;
+        this.durationWeeks = durationWeeks;
+    }
+    
+    // Getters
+    public Promotion getPromotion() { return promotion; }
+    public int getPayPerShow() { return payPerShow; }
+    public int getDurationWeeks() { return durationWeeks; }
 }
