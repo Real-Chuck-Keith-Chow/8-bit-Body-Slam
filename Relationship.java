f3f6a69b94a81dfaf6f25510d7a9a7f2f16198da
Cheuk (Chuck) Chow <rosarollins069@gmail.com>
2025-05-11 16:04:45 -0400

    Update Relationship.java

--- Relationship.java
+++ Relationship.java
@@ -1,8 +1,18 @@
 package wrestling.simulator;
+import java.io.Serializable;
 
-public class Relationship {
-	private Wrestler otherWrestler;
-    private int heatLevel; // -100 (hate) to +100 (best friend)
+public class Relationship implements Serializable {
+    private Wrestler otherWrestler;
+    private int heatLevel;
     private boolean isRomantic;
 
+    public Relationship(Wrestler otherWrestler, int heatLevel, boolean isRomantic) {
+        this.otherWrestler = otherWrestler;
+        this.heatLevel = heatLevel;
+        this.isRomantic = isRomantic;
+    }
+
+    public Wrestler getOtherWrestler() { return otherWrestler; }
+    public int getHeatLevel() { return heatLevel; }
+    public boolean isRomantic() { return isRomantic; }
 }
