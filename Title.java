b7cb2610abaf16d759aceafa898732ec26b5ebf6
Cheuk (Chuck) Chow <rosarollins069@gmail.com>
2025-05-11 16:02:17 -0400

    Update Title.java

--- Title.java
+++ Title.java
@@ -1,8 +1,18 @@
 package wrestling.simulator;
+import java.io.Serializable;
 
-public class Title {
-	  private String name; // "World Heavyweight"
-	    private Wrestler champion;
-	    private int prestige; // 0-100
+public class Title implements Serializable {
+    private String name;
+    private Wrestler champion;
+    private int prestige;
 
+    public Title(String name, int prestige) {
+        this.name = name;
+        this.prestige = prestige;
+    }
+
+    public String getName() { return name; }
+    public Wrestler getChampion() { return champion; }
+    public int getPrestige() { return prestige; }
+    public void setChampion(Wrestler champion) { this.champion = champion; }
 }
