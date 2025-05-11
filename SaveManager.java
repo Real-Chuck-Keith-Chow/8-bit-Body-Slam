e5502e5181cd5888172b3948d7bcba0b7fbdfea3
Cheuk (Chuck) Chow <rosarollins069@gmail.com>
2025-05-11 16:04:19 -0400

    Update SaveManager.java

--- SaveManager.java
+++ SaveManager.java
@@ -1,18 +1,17 @@
 package wrestling.simulator;
-
 import java.io.*;
 
 public class SaveManager {
     public static void saveGame(Wrestler player) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(
-                new FileOutputStream("career.sav"))) {  // Fixed: Added missing ")" and renamed variable
+                new FileOutputStream("career.sav"))) {
             oos.writeObject(player);
-        } // try-with-resources auto-closes the stream
+        }
     }
 
     public static Wrestler loadGame() throws IOException, ClassNotFoundException {
         try (ObjectInputStream ois = new ObjectInputStream(
-                new FileInputStream("career.sav"))) {  // Fixed: Added missing ")"
+                new FileInputStream("career.sav"))) {
             return (Wrestler) ois.readObject();
         }
     }
