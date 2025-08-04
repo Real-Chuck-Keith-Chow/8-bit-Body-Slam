3c300c8a57928d4eb75945a4f255f41322b135ca
Cheuk (Chuck) Chow <rosarollins069@gmail.com>
2025-08-04 15:31:25 -0400

    Update and rename src/main/java/model/Wrestler.java to src/wrestling/simulator/Wrestler.java

---
+++ src/wrestling/simulator/Wrestler.java
@@ -0,0 +1,47 @@
+package wrestling.simulator;
+
+public class Wrestler {
+	private String name;
+    private int health = 100;
+    private int popularity = 0;
+    private int money = 5000;
+    private int week = 0;
+
+    public Wrestler(String name) {
+        this.name = name;
+    }
+
+    // Getters & Setters
+    public String getName() { return name; }
+    public int getHealth() { return health; }
+    public int getPopularity() { return popularity; }
+    public int getMoney() { return money; }
+    public int getWeek() {return week;}
+
+	public void setMoney(int i) {
+		 this.money = Math.max(0, money);
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setHealth(int i) {
+		this.health = Math.min(100, Math.max(0, health));
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setPopularity(int i) {
+		  this.popularity = Math.max(0, popularity); // Prevent negative popularity
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setWeek(int i) {
+		
+		 this.week = Math.max(0, week); // Prevent negative weeks
+		// TODO Auto-generated method stub
+		
+	}
+
+}
